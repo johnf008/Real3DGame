dccef0c3406bf7aecf9d5a4ae3e06d47ce9f16b2
johnf008 <Carlos.flores.h108@gmail.com>
2025-06-23 11:18:26 -0500

    HOLY COW COLLISION WORKS
    I just needed another area 3d in the player

--- vanish_platform_one.gd
+++ vanish_platform_one.gd
@@ -5,4 +5,5 @@ func _on_area_3d_area_entered(area: Area3D) -> void:
 	if area.is_in_group("player"):
 		use_collision = false
 		transparency = 1.0
-	print("Collission :3")
+		print("Collision of group player")
+	print("Collission :3" + str(area))
