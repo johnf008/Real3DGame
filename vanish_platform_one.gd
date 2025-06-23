ae57aad9f17ec19f3018ebc824ceee97a61d2ca9
johnf008 <Carlos.flores.h108@gmail.com>
2025-06-23 12:06:54 -0500

    Separated the vanishing platform from main game scene & started working on animation for warning
    Took me too long ;-;

--- vanish_platform_one.gd
+++ vanish_platform_one.gd
@@ -3,7 +3,11 @@ extends CSGBox3D
 
 func _on_area_3d_area_entered(area: Area3D) -> void:
 	if area.is_in_group("player"):
-		use_collision = false
-		transparency = 1.0
+		%Platform_Vanish_Timer.start()
 		print("Collision of group player")
-	print("Collission :3" + str(area))
+	#print("Collission :3" + str(area))
+
+
+func _on_platform_vanish_timer_timeout() -> void:
+	use_collision = false
+	transparency = 1.0
