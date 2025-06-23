98054115d267e4a65c07ca9ee951be204889019f
johnf008 <Carlos.flores.h108@gmail.com>
2025-06-23 13:48:16 -0500

    Animation plays now, but now everything else that has the same color is playing the animation ;-; ;-;
    Solved a bug, but caused another bug to form ;-;

--- vanish_platform_one.gd
+++ vanish_platform_one.gd
@@ -2,11 +2,13 @@ extends CSGBox3D
 
 @onready var animation_tree: AnimationTree = %AnimationTree
 
+
 func _on_area_3d_area_entered(area: Area3D) -> void:
 	if area.is_in_group("player"):
+		warn()
 		%Platform_Vanish_Timer.start()
 		print("Collision of group player")
-	#print("Collission :3" + str(area))
+	print("Collission :3" + str(area))
 
 
 func _on_platform_vanish_timer_timeout() -> void:
