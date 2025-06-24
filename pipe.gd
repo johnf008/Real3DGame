4917509d48261c4eca3d8f675e8976f0f8f0377c
johnf008 <Carlos.flores.h108@gmail.com>
2025-06-23 21:28:02 -0500

    Idk what i did, but somewhere along the road i disconnected the signal that allowed the bat to take damage. but it's fixed now :3

--- pipe.gd
+++ pipe.gd
@@ -7,7 +7,8 @@ extends Node3D
 
 
 func _on_pipe_spawn_timer_timeout() -> void:
-	var new_mob = mob_to_spawn.instantiate()
-	add_child(new_mob)
-	new_mob.global_position = pipe_marker.global_position
-	print("Chat is this even executing?")
+	#var new_mob = mob_to_spawn.instantiate()
+	#add_child(new_mob)
+	#new_mob.global_position = pipe_marker.global_position
+	#print("Chat is this even executing?") yes
+	pass
